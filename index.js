1ea0c58414bb0fe5f2a987d2f16b835348ed7db8
Donovan Lambert <d.lambert@insolus.com>
2017-06-29 15:30:19 +0200

    Add source file

---
+++ index.js
@@ -0,0 +1,64 @@
+const path       = require('path');
+const babel      = require('rollup-plugin-babel');
+const babelrc    = require('babelrc-targeted-rollup');
+const commonjs   = require('rollup-plugin-commonjs');
+const resolve    = require('rollup-plugin-node-resolve');
+const progress   = require('rollup-plugin-progress');
+const uglify     = require('rollup-plugin-uglify');
+const cleanup    = require('rollup-plugin-cleanup');
+const { minify } = require('uglify-es');
+
+const _getPathWithCustomSuffx = (p, sffx) => {
+    const parts = path.parse(p);
+    parts.name  = `${parts.name}.${sffx}`;
+    delete parts.base;
+
+    return path.format(parts);
+};
+
+const generateTargetPlugins = target => [
+    progress(),
+    babel(Object.assign(babelrc(target), {
+        exclude: 'node_modules/**'
+    })),
+    resolve(),
+    commonjs(),
+    cleanup()
+];
+
+module.exports = function (baseOptions) {
+    const configs = [];
+    const targets = [
+        {
+            dest         : baseOptions.dest,
+            format       : 'umd',
+            withMinified : true,
+            moduleName   : baseOptions.moduleName,
+            plugins      : generateTargetPlugins('browsers')
+        },
+        {
+            dest    : _getPathWithCustomSuffx(baseOptions.dest, 'es'),
+            format  : 'es',
+            plugins : generateTargetPlugins('node')
+        }
+    ];
+    targets.forEach((targetOptions) => {
+        const options      = Object.assign({}, baseOptions, targetOptions);
+        const withMinified = options.withMinified || false;
+        delete options.withMinified;
+        configs.push(options);
+
+        if (!withMinified) {
+            return;
+        }
+
+        const minOptions = Object.assign({}, options, {
+            dest: _getPathWithCustomSuffx(targetOptions.dest, 'min')
+        });
+        minOptions.plugins = minOptions.plugins.slice(0);
+        minOptions.plugins.push(uglify({}, minify));
+        configs.push(minOptions);
+    });
+
+    return configs;
+};
